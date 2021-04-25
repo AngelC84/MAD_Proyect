7562d166e37222d43eff91f49dd6cb446f5a4561
sebastianidiot0 <83084622+sebastianidiot0@users.noreply.github.com>
2021-04-25 16:58:15 -0500

    Add files via upload

--- MAD_Proyect/Avance_Tablas.sql
+++ MAD_Proyect/Avance_Tablas.sql
@@ -12,6 +12,10 @@ IF EXISTS(SELECT 1 FROM sysconstraints WHERE OBJECT_NAME(constid) = 'fk_ClienteC
 BEGIN
 	ALTER TABLE Contrato DROP CONSTRAINT fk_ClienteCon;
 END
+IF EXISTS(SELECT 1 FROM sysconstraints WHERE OBJECT_NAME(constid) = 'fk_MedidorCon' AND OBJECT_NAME(id) = 'Contrato')
+BEGIN
+	ALTER TABLE Contrato DROP CONSTRAINT fk_MedidorCon;
+END
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -28,6 +32,10 @@ END
 IF EXISTS(SELECT 1 FROM sysconstraints WHERE OBJECT_NAME(constid) = 'fk_TarifaRec' AND OBJECT_NAME(id) = 'Recibo')
 BEGIN
 	ALTER TABLE Recibo DROP CONSTRAINT fk_TarifaRec;
+END
+IF EXISTS(SELECT 1 FROM sysconstraints WHERE OBJECT_NAME(constid) = 'fk_ServRec' AND OBJECT_NAME(id) = 'Recibo')
+BEGIN
+	ALTER TABLE Recibo DROP CONSTRAINT fk_ServRec;
 END
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -118,7 +126,8 @@ END
 
 CREATE TABLE Administrador(
 Nombre_Usuario			VARCHAR(50)  PRIMARY KEY,
-Contraseña				VARCHAR (15)
+Contraseña				VARCHAR (15),
+Activo bit
 );
 
 
@@ -127,6 +136,7 @@ Contraseña				VARCHAR (15)
 CREATE TABLE Usuarios(
 Nombre_Usuario	VARCHAR (50) PRIMARY KEY,
 Contraseña		VARCHAR (15)
+
 );
 
 
@@ -138,6 +148,8 @@ RFC VARCHAR (12),
 Fecha_de_Alta_Modificacion date,
 Nombre_Usuario VARCHAR(50),
 CONSTRAINT fk_Usuario FOREIGN KEY (Nombre_Usuario) REFERENCES Usuarios (Nombre_Usuario),
+Activo bit
+
 );
 
 
@@ -147,10 +159,23 @@ CURP VARCHAR (18) PRIMARY KEY,
 Fecha_Nacimiento date,
 Nombre VARCHAR (50),
 Fecha_de_Alta_Modificacion date,
-Genero VARCHAR (50),
-Domicilio VARCHAR (60),
+Activo bit
 
 );
+
+
+
+
+CREATE TABLE Consumo(
+Numero_Medidor int PRIMARY KEY IDENTITY (153458,1),
+Watts int,
+Periodo date,
+Cliente VARCHAR(18),
+CONSTRAINT fk_ClienteCons FOREIGN KEY (Cliente) REFERENCES Clientes (CURP),
+
+);
+
+
 
 
 CREATE TABLE Tarifa(
@@ -160,23 +185,11 @@ Precio_Watt_Medio int,
 Precio_Watt_Excedente int,
 Fecha date,
 Tipo_de_uso bit,
-Genero VARCHAR (50),
-Domicilio VARCHAR (60),
-);
 
-
-CREATE TABLE Consumo(
-Numero_Medidor int PRIMARY KEY IDENTITY (153458,1),
-Domicilio VARCHAR (70),
-Watts int,
-Periodo date,
-Cliente VARCHAR(18),
-CONSTRAINT fk_ClienteCons FOREIGN KEY (Cliente) REFERENCES Clientes (CURP),
 
 );
-
 CREATE TABLE Servicio(
-Numero_de_Servicio	int PRIMARY KEY,
+Numero_de_Servicio	int  PRIMARY KEY,
 Tipo_de_Servicio		VARCHAR (15),
 Numero_Medidor int,
 CONSTRAINT fk_MedidorServ FOREIGN KEY (Numero_Medidor) REFERENCES Consumo (Numero_Medidor),
@@ -185,15 +198,30 @@ CONSTRAINT fk_TarifaServ FOREIGN KEY (Tarifa) REFERENCES Tarifa (Id_Tarifa),
 Cliente VARCHAR(18),
 CONSTRAINT fk_ClienteServ FOREIGN KEY (Cliente) REFERENCES Clientes (CURP),
 );
+CREATE TABLE Contrato(
+Domicilio VARCHAR (50) PRIMARY KEY ,
+Servicio int,
+CONSTRAINT fk_ServicioCon FOREIGN KEY (Servicio) REFERENCES Servicio (Numero_de_Servicio),
+Fecha date,
+Cliente VARCHAR (18),
+CONSTRAINT fk_ClienteCon FOREIGN KEY (Cliente) REFERENCES Clientes (CURP),
+Activo bit,
+Numero_Medidor int,
+CONSTRAINT fk_MedidorCon FOREIGN KEY (Numero_Medidor) REFERENCES Consumo (Numero_Medidor),
 
+
+
+);
 CREATE TABLE Recibo(
 Id_Recibo int identity (11564645,1) Primary Key,
-Tipo_de_Servicio		VARCHAR (15),
+
 Iva tinyint,
 Fecha date,
 Watts int,
 Numero_Medidor int,
 CONSTRAINT fk_MedidorRec FOREIGN KEY (Numero_Medidor) REFERENCES Consumo (Numero_Medidor),
+Numero_de_Servicio		int,
+CONSTRAINT fk_ServRec FOREIGN KEY (Numero_de_Servicio) REFERENCES Servicio (Numero_de_Servicio),
 Cliente VARCHAR(18),
 CONSTRAINT fk_ClienteRec FOREIGN KEY (Cliente) REFERENCES Clientes (CURP),
 Tarifa VARCHAR(20),
@@ -202,15 +230,3 @@ Subtotal int,
 Total int
 );
 
-CREATE TABLE Contrato(
-Id_Contrato int IDENTITY (1465565786,1)PRIMARY KEY ,
-Domicilio VARCHAR (50),
-Servicio int,
-CONSTRAINT fk_ServicioCon FOREIGN KEY (Servicio) REFERENCES Servicio (Numero_de_Servicio),
-Fecha date,
-Cliente VARCHAR (18),
-CONSTRAINT fk_ClienteCon FOREIGN KEY (Cliente) REFERENCES Clientes (CURP),
-Activo bit
-
-
-);
